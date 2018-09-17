9a0019c6f319619fafd9693d22fc6b40320d3ee4
Rashika Patel <rashika_patel@hotmail.com>
2018-09-17 17:05:20 +0100

    add test for note list

--- note-list-tests.js
+++ note-list-tests.js
@@ -1,9 +1,7 @@
 (function(exports) {
   function noteListTest() {};
-    noteListTest.prototype.test = function() {
-      var l1 = new NoteList();
-      console.log(l1.list)
-      if (l1.list.length === 0) {
+    noteListTest.prototype.test = function(noteList) {
+      if (noteList.list.length === 0) {
         console.log("new empty note list") 
       }
       else {
